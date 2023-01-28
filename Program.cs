39d6e9f8d04c68aa3111798977c813941e955219
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-01-28 11:38:28 +0530

    Added Files :

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To EmployeeWage Computation :");
+        }
+    }
+}
