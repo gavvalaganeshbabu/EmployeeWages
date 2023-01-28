464dd786310e4028f7a9b8b3cd409e4e94e51d52
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-01-28 13:45:55 +0530

    Added Code

--- Program.cs
+++ Program.cs
@@ -7,7 +7,29 @@ namespace EmployeeWages
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To EmployeeWage Computation :");
-                     
+            Console.WriteLine("Calculating Full-Time And  Part-Time Wages :");
+            Console.WriteLine("Enter Employee Name :");
+            Random rnd = new Random();
+            string name = Console.ReadLine();
+            int n = rnd.Next(0, 3);
+            int WagePerHour = 20;
+            int FullDayHour = 8;
+            int PartTimeHour = 4;
+            if (n == 0)
+            {
+                Console.WriteLine("Employee is Absent Today : ");
+                Console.WriteLine(name + " Wages = 0 $");
+            }
+            else if (n == 1)
+            {
+                Console.WriteLine(name + " is Present FullDay :");
+                Console.WriteLine(name + " Wage = " + WagePerHour * FullDayHour + " $");
+            }
+            else
+            {
+                Console.WriteLine(name + " is Present Today and Worked partime :");
+                Console.WriteLine("Employee Wage = " + WagePerHour * PartTimeHour + " $");
+            }
         }      
     }
 }
