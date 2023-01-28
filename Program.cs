62195373b0cb9a5d61f661101a2fd9ea332021a9
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-01-28 11:46:34 +0530

    Added UC1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ namespace EmployeeWages
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To EmployeeWage Computation :");
+            Console.WriteLine("Enter the Employee Name :");
+            string name=Console.ReadLine(); 
+            Random rnd =new Random();
+            int n=rnd.Next(0,2);
+            if (n == 0)
+            {
+                Console.WriteLine(name + " Is Absent Today");
+            }
+            else { 
+            Console.WriteLine(name+" is Present Today");
+            }
         }
     }
 }
